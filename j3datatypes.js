5f4e27463322b35c58bb64e48ddffa83bae53962
Sukhpreet singh <sukhpreet9808@gmail.com>
2024-07-27 16:51:43 +0530

    stack & heap memory summary

--- j3datatypes.js
+++ j3datatypes.js
@@ -45,23 +45,80 @@
 
 // array
 
- const DC = ["soanpari", "shaktimaan", "power rangers"];
+//  const DC = ["soanpari", "shaktimaan", "power rangers"];
 
 //  objects 
 
-const objs = {
-    name: "sukhman",
-    age: 20, 
-    enrolnum: 1181,
-    isloggedin: true,
-}
+// const objs = {
+//     name: "sukhman",
+//     age: 20, 
+//     enrolnum: 1181,
+//     isloggedin: true,
+// }
 
 // functions 
 
-const ismyfunc = function(){
- console.log("value");
+// const ismyfunc = function(){
+//  console.log("value");
+// }
+
+// ismyfunc();
+
+// console.log(typeof (DC))
+
+
+// stack ( primitive  types) 
+// :- means whenver stack memory is used then  the variable we  declared we get the copy of it.
+
+//  and heap memory  (non - primitve types)
+// :- when heap memory got used means we got the reference to the original declared , so the changes are done in reference also get changes in original value
+
+ 
+// stack memory example :-
+let myytname = "sukhiiyt"; // this goes in stack memory
+ 
+let anotheryt = myytname;  // here copy is created in stack not original value ref is passed 
+anotheryt = "pythonyt";
+
+console.log(anotheryt);
+console.log(myytname);
+
+
+// heap memory 
+
+let user1 = {
+     email: "sukh8954@google.com",
+     upiId : "89547@pytes",
 }
 
-ismyfunc();
 
-console.log(typeof (DC))
+// user1 get direct ref to the data defined in the 
+// object which is stored in heap memory. 
+
+let user2 = user1; // variable declared in stack but
+// value it get it from the reference of og values of user1
+
+// now whatever changes done in user2 will reflect in user1
+
+user2.email = "sukhpreet9808@gmail.com";
+
+console.log(user1.email);
+console.log(user2.email);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
