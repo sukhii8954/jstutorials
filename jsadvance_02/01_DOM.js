212007b43ee56e65c06e0d296a43655e45679791
Sukhpreet singh <sukhpreet9808@gmail.com>
2024-08-16 18:38:48 +0530

    DOM_01

---
+++ jsadvance_02/01_DOM.js
@@ -0,0 +1,7 @@
+
+// console.log(window.document) shows only html body of that window document
+// if we use (dir = directory) console.dir(window.document) , if want more information about that page.
+
+// with DOM mamupulation we can get Document ID and can change context , modify it , can add new elements , we can do filtering also  
+// Example:- in js
+// document.getElementById('idname').innerHTML = "whatever we want to do here"
