49852e93cd9cb8a9a08c4986e5fe08d726bf0a38
Sukhpreet singh <sukhpreet9808@gmail.com>
2024-08-27 15:26:52 +0530

    dom day two

---
+++ jsadvance_02/02_DOM.js
@@ -0,0 +1,68 @@
+//  create a new element in DOM --> relations 
+
+// const parent = document.querySelector('.parent')
+// console.log(parent);
+// console.log(parent.children);  
+
+// we get html collection which is type of array
+// console.log(parent.children[1].innerHTML);
+
+// for(let i =0 ; i <parent.children.length; i++){
+//     console.log(parent.children[i].innerHTML);
+// }
+
+// parent.children[1].style.color = "aqua";
+// console.log(parent.firstElementChild);
+// console.log(parent.lastElementChild);
+
+
+// getting parent from child
+// const dayOne =  document.querySelector('.day');
+// console.log(dayOne);  
+// console.log(dayOne.parentElement);
+// console.log(dayOne.nextElementSibling); 
+// first child was monday next will be tuesday
+
+
+
+
+// console.log("NODES: ",parent.childNodes);
+
+
+// *******************************************************
+
+// --> creating  elements in js
+
+const div = document.createElement('div')
+console.log(div);
+const heading = document.createElement('h1')
+console.log(heading);
+
+
+// adding properties in this div element
+
+div.className = "main"
+div.id = Math.round(Math.random() *10+1) // unique no would be assign as ID
+
+
+// custom attributes set aise krte h :-
+
+div.setAttribute("title", "chai aur code");
+div.style.backgroundColor="green";
+div.style.padding = "20px";
+
+// NOTE:-
+//  Attaching an element to DOM page to display:- 
+
+// div.innerText = "chai aur code";
+
+//  OR
+
+ const addText =document.createTextNode("chai our Code");
+ div.appendChild(addText);
+
+ document.body.appendChild(div);
+
+
+//  we can wrap the above whole div in function and 
+// can pass arguments in it 
--- test.cpp
+++ test.cpp
@@ -25,24 +25,56 @@ using namespace std;
 // }
 // }
 
-int getlength(char name[])
-{
+// int getlength(char name[])
+// {
 
-  int count = 0;
-  for (int i = 0; name[i] != '\0'; i++)
-  {
-    count++;
-  }
-  return count;
-}
+//   int count = 0;
+//   for (int i = 0; name[i] != '\0'; i++)
+//   {
+//     count++;
+//   }
+//   return count;
+// }
+
+
+    int totalFruits(vector<int> &arr) {
+          
+          int i =0, j=0;
+          int maxlen = 0; 
+          unordered_map<int ,int> freq;
+              
+              int n = arr.size();
+           while(j< n){
+               freq[arr[j]]++;
+                  
+                  if(freq.size()>2){    
+                        freq[arr[i]]--;
+                          if(freq[arr[i]]==0){
+                            freq.erase(arr[i]);
+                          } 
+                          i++;
+                   }
+                    else if(freq.size()<=2){
+                         maxlen = max(maxlen,j-i+1); 
+                  }   
+                   j++; 
+           }
+      return maxlen;
+    }
+
 int main()
 {
-  char name[20];
-  cin >> name;
+   vector<int> arr;
+    int n ;
+    cout<< "enter size of array: "; 
+    cin >> n;
+   cout<<"Enter the elements in arr: ";
+   for(int i = 0 ; i< n; i++ ){
+      cin >> arr[i];
+   }
 
-  cout << "The length of the string is " << getlength(name);
-  //   vector<int> arr = {2,1,2,3,3,3,3};
-  //    int ans =findmajor(arr);
-  //    cout<<ans;
+   int ans = totalFruits(arr);
+   cout <<ans ;
+
   return 0;
 }
