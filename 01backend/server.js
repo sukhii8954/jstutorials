81d5067526e0db6edf8db0baabd992ca85e60cce
Sukhpreet singh <sukhpreet9808@gmail.com>
2024-12-24 13:08:57 +0530

    frontend&backend

---
+++ 01backend/server.js
@@ -0,0 +1,49 @@
+import express from 'express';  // module js import
+//   include type : module in package to use module js
+const app = express();
+
+
+app.get('/', (req, res)=> {
+    res.send('server is ready');
+});
+
+// get a list of 5 jokes
+
+app.get('/jokes', (req, res)=> {
+    const jokes = [
+        {
+            id:1,
+            title: 'A joke',
+            content: '18+ joke : nineteen',
+        },
+        {
+            id:2,
+            title: 'Another joke',
+            content: 'this is another joke',
+        },
+        {
+            id:3,
+            title: 'third joke',
+            content: 'this is third joke',
+        },
+        {
+            id:4,
+            title: 'dank joke',
+            content: 'this is dank joke like samay raina',
+        },
+        {
+            id:5,
+            title: 'nerd joke',
+            content: 'carryminati joke',
+        },
+    ]
+   
+    res.send(jokes);
+
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> {
+    console.log(`server at http://localhost:${port}`);
+});
